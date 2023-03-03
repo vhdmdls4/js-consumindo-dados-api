db8ceb9081299519880a0c52f6655240920be719
Victor Hugo Martins Duarte Lima <vhduartelima@hotmail.com>
2023-03-03 20:05:17 -0300

    commiting notes

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/');
+async function data() {
+  const consultaCEP = fetch("https://viacep.com.br/ws/35330000/json/", {})
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => console.log(err));
+}
 
-console.log(consultaCEP);
+data();
+
+//or
+
+const verCEP = fetch("https://viacep.com.br/ws/35330000/json/", {})
+  .then((response) => response.json())
+  .then((response) => {
+    if (response.erro) {
+      throw Error("CEP não deu bom");
+    } else console.log(response);
+  })
+  .catch((err) => console.log(err))
+  .finally((message) => console.log("Successful"));
+
+//fetch é um comando assíncrono que executa uma promise que tem
+//um resolve e um reject (dois callbacks), caso dê errado, reject;
+//Api é basicamente uma aplicação que faz a conexão entre cliente,
+//que faz requisição/solicitação, e servidor, que manda resposta
+// fetch("https://viacep.com.br/ws/01001000/json/",{
+//   method: 'get',
+//   headers:{},
+// }).then((response)=>{
+//   if(!response.ok){
+//     console.log("Problema no server")
+//   } else{
+//     const data = response.json()
+//     return data;
+
+//   }
+// })
