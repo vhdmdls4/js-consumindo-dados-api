cb802f739b0c3e8fc51dfe3c1fe50b17c587c15a
Victor Hugo Martins Duarte Lima <vhduartelima@hotmail.com>
2023-04-17 15:26:13 -0300

    ending the lessons

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+async function searchAdress(cep) {
+  var errorWarning = document.getElementById("erro");
+  errorWarning.innerHTML = "";
+  try {
+    var searchCEP = await fetch(`https://viacep.com.br/ws/${cep}/json`);
+    var searchCEPconverted = await searchCEP.json();
+    if (searchCEPconverted.erro) {
+      throw Error("CEP não existente");
+    }
+    var adress = document.querySelector('input[name="endereco"]');
+    var borough = document.querySelector('input[name="bairro"]');
+    var city = document.querySelector('input[name="cidade"]');
+    var state = document.querySelector('select[name="estado"]');
+
+    adress.value = searchCEPconverted.logradouro;
+    borough.value = searchCEPconverted.bairro;
+    city.value = searchCEPconverted.localidade;
+    state.value = searchCEPconverted.uf;
+
+    console.log(searchCEPconverted);
+
+    return searchCEPconverted;
+  } catch (err) {
+    errorWarning.innerHTML = `<p> CEP inválido, tente novamente! </p>`;
+    console.log(err);
+  }
+}
+
+var cep = document.getElementById("cep");
+
+cep.addEventListener("focusout", () => {
+  searchAdress(cep.value);
+});
+
+// let CEPArray = ["01001000", "30330000", 31310350];
+// let allCEPs = [CEPArray.map((valores) => searchAdress(valores))];
+// Promise.all(allCEPs).then((response) => console.log(response));
+
+/*
+//await acima só funciona dentro de uma async function, await async só funciona com uma promisse por vez.
+//Para saber: promises usando o .then() são processadas em paralelo e, portanto, mais rápidas.
+//Já o método async/await é um método mais palatável do ponto de vista da legibilidade do código, syntax sugar.
+//https://www.alura.com.br/artigos/async-await-no-javascript-o-que-e-e-quando-usar
+//muitos callbacks function, forma acima evita isso
 async function data() {
   const consultaCEP = fetch("https://viacep.com.br/ws/35330000/json/", {})
     .then((response) => response.json())
